7f0655206447b919f88ceee610789ade2213ee19
aswathytp <aswathytp44@gmail.com>
2022-12-02 01:53:02 +0530

    added

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+import sklearn
+
+app = Flask(__name__)
+model = pickle.load(open('saved.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+def process(form_data):
+    data = pd.Series(form_data)
+    continuous_column = ['salary','product']
+    for index,value in data.iteritems():
+        if index in continuous_column:
+            if len(value)==0:
+                value = 0
+            data[index] = float(value)
+    df = pd.DataFrame(data=[data.values],columns=data.index)
+    return df
+
+@app.route('/predict',methods=['POST'])
+def predict():
+  
+    df = process(request.form)
+    prediction = model.predict(df)[0]
+    return render_template('predict.html', prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8000)
---
+++ model.py
@@ -0,0 +1,41 @@
+"""PRML_Course_Project.ipynb
+"""
+import sys
+import warnings
+import pandas as pd
+import numpy as np
+import pickle
+import warnings
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('mytable.csv')
+clf = LogisticRegression()
+
+# selected features
+selected_features= ['salary','Product']
+
+X = df[selected_features]
+y = df['purchased']
+
+categorical_cols = [col for col in X.columns if X[col].nunique() < 10 and X[col].dtype == 'object']
+
+numerical_cols = [col for col in X.columns if X[col].dtype in ['int64', 'float64']]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_cols),
+        ('num', StandardScaler(), numerical_cols)
+    ])
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('classifier', clf)
+                              ])
+
+
+my_pipeline.fit(X, y)
+
+pickle.dump(my_pipeline, open('saved.pkl','wb'))
